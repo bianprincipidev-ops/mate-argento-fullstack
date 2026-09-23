c1bafd516da0625430adfbbe79c4ee22c0abf8b9
bianprincipidev-ops <bianprincipi.dev@gmail.com>
2026-09-23 19:30:22 -0300

    fix: config url dinamica para conectar api en produccion y desarrollo

--- frontend/src/productoService.js
+++ frontend/src/productoService.js
@@ -1,4 +1,6 @@
-const API_URL = 'http://127.0.0.1:8000/api/productos/';
+const API_URL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
+    ? 'http://127.0.0.1:8000/api/productos/'
+    : 'https://mate-argento.com.ar/api/productos/'; // Ajusta esta URL si tu backend de Django en Hostinger corre en otra ruta o subdominio
 
 export const obtenerProductos = async () => {
     try {
